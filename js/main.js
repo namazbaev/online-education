de9e496d5ef242857cf6dbc42be658e58aa69046
shakxzod_namazbaev <shaxkzod.namazbaev.9700@gmail.com>
2021-12-23 08:55:52 +0500

    content section

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ menu.onclick = function(){
     menu.classList.toggle('fa-times')
     header.classList.toggle('active')
 }
+window.onscroll = function(){
+    menu.classList.remove('fa-times')
+    header.classList.toggle('active')
+}
